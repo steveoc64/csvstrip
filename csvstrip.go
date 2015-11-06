29515f00160dfa932ffe1f66e67168100d37525a
steveoc64 <steveoc64@gmail.com>
2015-11-06 13:12:21 +1030

    Strip out CR & LF if inside quotes

--- csvstrip.go
+++ csvstrip.go
@@ -13,7 +13,50 @@ func main() {
 
 	log.Println("Stripping", infile, "to", outfile)
 
-	if contents, err := ioutil.ReadFile(infile); err != nil {
-		log.Fatalln("Error:", e.Error)
+	contents, err := ioutil.ReadFile(infile)
+	if err != nil {
+		log.Fatalln("Error:", err.Error)
 	}
+	log.Println("Read in ", len(contents), "bytes from", infile)
+
+	inquotes := false
+	linenumber := 1
+	killed := 0
+	outputbytes := make([]byte, 0, len(contents))
+	//var output []byte
+
+	// Apply the stripper algorithm to the input bytes
+	for _, b := range contents {
+		if b == '"' {
+			inquotes = !inquotes
+		}
+		switch b {
+		case '\r':
+			if inquotes {
+				log.Println("CR in quotes on line", linenumber)
+				killed++
+			}
+			break
+		case '\n':
+			if inquotes {
+				log.Println("LF in quotes on line", linenumber)
+				killed++
+			} else {
+				log.Println("New line", linenumber)
+				outputbytes = append(outputbytes, b)
+			}
+			linenumber++
+			break
+		default:
+			outputbytes = append(outputbytes, b)
+			break
+		}
+	}
+
+	// Now write the output bytes to the output file
+	err = ioutil.WriteFile(outfile, outputbytes, 0777)
+	if err != nil {
+		log.Println("WriteFile :", err.Error())
+	}
+	log.Println("Ended up killing", killed, "bytes")
 }
